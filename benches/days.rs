95d2515bf2b55944db18732a82dd25bec93c710e
Daniel Boyle <boylede+github@gmail.com>
2022-12-04 00:42:46 -0500

    add benchmarks

--- benches/days.rs
+++ benches/days.rs
@@ -10,5 +10,15 @@ fn day2(c: &mut Criterion) {
     c.bench_function("day 02", |b| b.iter(|| run(black_box(INPUT))));
 }
 
-criterion_group!(benches, day1, day2);
+fn day3(c: &mut Criterion) {
+    use aoc2022::days::day3::*;
+    c.bench_function("day 03", |b| b.iter(|| run(black_box(INPUT))));
+}
+
+fn day4(c: &mut Criterion) {
+    use aoc2022::days::day4::*;
+    c.bench_function("day 04", |b| b.iter(|| run(black_box(INPUT))));
+}
+
+criterion_group!(benches, day1, day2, day3, day4);
 criterion_main!(benches);
--- src/days/day4.rs
+++ src/days/day4.rs
@@ -2,7 +2,7 @@ pub const INPUTS: &[&str] = &[INPUT];
 pub const INPUT: &str = include_str!("../../input/day4.txt");
 
 pub fn run(input: &str) -> (String, String) {
-    let parta = input
+    let ranges: Vec<(i32, i32, i32, i32)> = input
         .lines()
         .map(|line| {
             let (a, b) = line.split_once(',').unwrap();
@@ -15,24 +15,17 @@ pub fn run(input: &str) -> (String, String) {
                 b_max.parse::<i32>().unwrap(),
             )
         })
+        .collect();
+
+    let parta = ranges
+        .iter()
         .filter(|(a_min, a_max, b_min, b_max)| {
             (a_min <= b_min && a_max >= b_max) || (b_min <= a_min && b_max >= a_max)
         })
         .count();
 
-    let partb = input
-        .lines()
-        .map(|line| {
-            let (a, b) = line.split_once(',').unwrap();
-            let (a_min, a_max) = a.split_once('-').unwrap();
-            let (b_min, b_max) = b.split_once('-').unwrap();
-            (
-                a_min.parse::<i32>().unwrap(),
-                a_max.parse::<i32>().unwrap(),
-                b_min.parse::<i32>().unwrap(),
-                b_max.parse::<i32>().unwrap(),
-            )
-        })
+    let partb = ranges
+        .iter()
         .filter(|(a_min, a_max, b_min, b_max)| {
             (a_min <= b_min && a_max >= b_min)
                 || (a_min <= b_max && a_max >= b_max)
