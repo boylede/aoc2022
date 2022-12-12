0e88ba57d49641cbf257c4c80b963130f6cede72
Daniel Boyle <boylede+github@gmail.com>
2022-12-12 17:55:12 -0500

    day9 part2

--- src/days/day9.rs
+++ src/days/day9.rs
@@ -1,6 +1,8 @@
 use std::collections::HashSet;
 
-pub const INPUTS: &[&str] = &[INPUT, INPUT_A];
+use itertools::Itertools;
+
+pub const INPUTS: &[&str] = &[INPUT, INPUT_A, INPUT_B];
 pub const INPUT: &str = include_str!("../../input/day9.txt");
 
 pub const INPUT_A: &str = "R 4
@@ -12,9 +14,18 @@ D 1
 L 5
 R 2";
 
+pub const INPUT_B: &str = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
+
 #[inline]
 pub fn run(input: &str) -> (String, String) {
-    let mut positions = input
+    let route: Vec<_> = input
         .lines()
         .flat_map(|line| {
             let (a, b) = line.split_once(' ').unwrap();
@@ -27,28 +38,50 @@ pub fn run(input: &str) -> (String, String) {
                 _ => unimplemented!(),
             }
         })
-        .scan(((0, 0), (0, 0)), |(head, tail), motion| {
-            head.0 += motion.0;
-            head.1 += motion.1;
-            // println!("moved head to {head:?}");
+        .collect();
+    let parta = {
+        let mut positions = route
+            .iter()
+            .scan(((0, 0), (0, 0)), |(head, tail), motion| {
+                head.0 += motion.0;
+                head.1 += motion.1;
+                update_tail(head,tail);
+                Some((tail.0, tail.1))
+            })
+            .collect::<HashSet<_>>();
+        positions.insert((0, 0));
+        positions.len()
+    };
 
-            let w: i32 = head.0 - tail.0;
-            let h: i32 = head.1 - tail.1;
-            if w.abs() > 1 {
-                tail.0 += w.signum();
-                tail.1 += h.signum();
-                
-            } else if h.abs() > 1 {
-                tail.0 += w.signum();
-                tail.1 += h.signum();
-            }
-            // println!("tail is at {tail:?}");
-            Some((tail.0, tail.1))
-        })
-        .collect::<HashSet<_>>();
-    positions.insert((0, 0));
+    let partb = {
+        let mut positions = route
+            .iter()
+            .scan([(0, 0); 10], |state, motion| {
+                state[0].0 += motion.0;
+                state[0].1 += motion.1;
+                for i in 0..(state.len()-1) {
+                    let head = state[i];
+                    let tail = &mut state[i+1];
+                    update_tail(&head,tail);
+                }
+                Some(state[9])
+            })
+            .collect::<HashSet<_>>();
 
-    let parta = positions.len();
-    let partb = 0;
+        positions.insert((0, 0));
+        positions.len()
+    };
     (format!("{parta}"), format!("{partb}"))
+}
+
+fn update_tail(head: &(i32, i32), tail: &mut (i32,i32)) {
+    let w: i32 = head.0 - tail.0;
+                let h: i32 = head.1 - tail.1;
+                if w.abs() > 1 {
+                    tail.0 += w.signum();
+                    tail.1 += h.signum();
+                } else if h.abs() > 1 {
+                    tail.0 += w.signum();
+                    tail.1 += h.signum();
+                }
 }
